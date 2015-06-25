15fe1a2ee28ab29c15da9d1a3df229e5c6cf5b63
a9 <themaxaxis@gmail.com>
2015-06-25 15:31:30 -0400

    added urban dictionary

---
+++ chat_modules/dictionary.js
@@ -0,0 +1,24 @@
+var client = require("../methods");
+var request = require("request");
+request.encoding = "UTF-8";
+var cheerio = require("cheerio");
+
+
+client.chat.on("message", function(ev, msg) {
+	var match = msg.match(/\.dict (.+)/);
+	if (match !== null) {
+		var word = match[1];
+		client.startTyping(ev);
+		request("https://www.oxforddictionaries.com/us/definition/american_english/" + encodeURI(word), function(error, response, body) {
+			if (!error) {
+				var $ = cheerio.load(body);
+				var definition = $(".definition").first().text();
+				if(definition !== "") client.replyMessage(ev, definition);
+				else client.replyMessage(ev, "No definitions found.");
+			} else {
+				client.replyMessage(ev, "error");
+			}
+			client.stopTyping(ev);
+		});
+	}
+});
