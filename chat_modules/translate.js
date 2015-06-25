690f701f072ca52692554b162d2ed55fac48e11d
William Tan <williamjefftan@gmail.com>
2015-06-24 21:17:48 -0400

    fixed translate issue with unicode characters

--- chat_modules/translate.js
+++ chat_modules/translate.js
@@ -1,17 +1,21 @@
 var client = require("../methods");
 var request = require("request");
+var iconv = require('iconv-lite');
 var cheerio = require("cheerio");
 
 
 client.chat.on("message", function(ev, msg) {
 	var match = msg.match(/\.t (.+)/);
-	if (match !== null) {
+	if (msg.indexOf(".t") === 0 && msg.length > 3) {
 		client.startTyping(ev);
-		var query = match[1];
-		request("https://translate.google.com/?text=" + query, function(error, response, body) {
+		var query = msg.substring(3);
+		console.log(query);
+		request.encoding = 'UTF-8'
+		request("https://translate.google.com/?text=" + encodeURI(query), function(error, response, body) {
 			if (!error) {
 				var $ = cheerio.load(body);
 				var translated = $("#result_box").text();
+				console.log(translated)
 				client.replyMessage(ev, translated);
 			} else {
 				client.replyMessage(ev, "error");
