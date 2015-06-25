24762eda316c1d25ff2dcfbd92893492a08ff6a8
Ninja3047 <Ninja3047@users.noreply.github.com>
2015-06-24 21:22:28 -0400

    removed console.logs

--- chat_modules/translate.js
+++ chat_modules/translate.js
@@ -9,13 +9,11 @@ client.chat.on("message", function(ev, msg) {
 	if (msg.indexOf(".t") === 0 && msg.length > 3) {
 		client.startTyping(ev);
 		var query = msg.substring(3);
-		console.log(query);
 		request.encoding = 'UTF-8'
 		request("https://translate.google.com/?text=" + encodeURI(query), function(error, response, body) {
 			if (!error) {
 				var $ = cheerio.load(body);
 				var translated = $("#result_box").text();
-				console.log(translated)
 				client.replyMessage(ev, translated);
 			} else {
 				client.replyMessage(ev, "error");
