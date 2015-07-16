178fa2b8222eb8cecf351219d2c9f0d1d17da032
Max Krieger <themaxaxis@gmail.com>
2015-07-16 18:37:17 +0100

    add subreddit module

---
+++ chat_modules/subreddit.js
@@ -0,0 +1,23 @@
+var client = require("../methods");
+var request = require("request");
+var cheerio = require("cheerio");
+
+
+client.chat.on("message", function(ev, msg) {
+	var match = msg.match(/r\/(.+)/);
+	if (match !== null) {
+		client.startTyping(ev);
+		var sub = match[1];
+		request("https://reddit.com/r/" + sub, function(error, response, body) {
+			if (!error) {
+				var $ = cheerio.load(body);
+				var description = $("title").text();
+				var linkbuilder = new client.Client.MessageBuilder();
+				var segments = linkbuilder.link("/r/" + sub + " - " + description, "https://reddit.com/r/" + sub).toSegments();
+			} else {
+				client.replyMessage(ev, "error");
+			}
+			client.stopTyping(ev);
+		});
+	}
+});
