bc46b5b618b28e7077a72779c24896233c31d80c
Max <themaxaxis@gmail.com>
2015-08-13 21:42:18 -0400

    added linkage

--- chat_modules/wiki.js
+++ chat_modules/wiki.js
@@ -3,13 +3,16 @@ var request = require("request");
 
 client.chat.on("message", function(ev, msg) {
 	var match = msg.match(/\.wiki (.+)/);
-	if (match ) {
+	if (match) {
 		client.startTyping(ev);
 		request("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exchars=420&explaintext&redirects=&titles=" + match[1], function(error, response, body) {
 			var res = JSON.parse(body);
-      var summary = res.query.pages[Object.keys(res.query.pages)[0]].extract;
-      if(summary === undefined) client.replyMessage(ev, "no results found");
-      else client.replyMessage(ev, summary);
+			var link = "https://en.wikipedia.org/wiki/" + match[1];
+			var summary = res.query.pages[Object.keys(res.query.pages)[0]].extract;
+			var linkbuilder = new client.Client.MessageBuilder();
+			var segments = linkbuilder.link(summary, link).toSegments();
+			if(summary) client.replySegments(ev, segments);
+			else client.replyMessage(ev, "no results found");
 			client.stopTyping(ev);
 		});
 	}
