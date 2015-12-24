4ed81a818827530feb9a632b8963876b9363a326
leedia <edianne98@gmail.com>
2015-12-24 17:49:54 -0500

    command
    adding .ascii is more user friendly

--- chat_modules/ascii.js
+++ chat_modules/ascii.js
@@ -9,7 +9,7 @@ client.chat.on("message", function(ev, msg) {
 	if (match) art = ascii[match[1].toLowerCase()];
 	if (match && art) {
 		client.replyMessage(ev, art);
-	} else if (msg == ".a") {
+	} else if ((msg == ".a") || (msg == ".ascii")) {
 		client.replyMessage(ev, Object.keys(ascii).reduce(function(p, c) {
 			return p + ", " + c;
 		}));
