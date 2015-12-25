f03a5c8edcc908fb657e6b104a3acaab9a77c090
leedia <edianne98@gmail.com>
2015-12-25 13:04:27 -0500

    NO U

--- chat_modules/ascii.js
+++ chat_modules/ascii.js
@@ -1,11 +1,12 @@
 var client = require("../methods");
+var request = require("request");
 
 var ascii = require("../config").ascii;
 
 client.chat.on("message", function(ev, msg) {
-	var match = msg.match(/\.a(scii)? (.+)/);
+	var match = msg.match((/\.a (.+)/)|(/\.ascii (.+)));
 	var art;
-	if (match) art = ascii[match[2].toLowerCase()];
+	if (match) art = ascii[match[1].toLowerCase()];
 	if (match && art) {
 		client.replyMessage(ev, art);
 	} else if (msg == ".a" || msg == ".ascii") {
