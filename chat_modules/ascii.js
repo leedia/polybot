a982ca1675116c70116d1ab77f3db29e5dbca7a6
Max Krieger <themaxaxis@gmail.com>
2015-12-25 13:02:33 -0500

    re-added .ascii support

--- chat_modules/ascii.js
+++ chat_modules/ascii.js
@@ -1,15 +1,14 @@
 var client = require("../methods");
-var request = require("request");
 
 var ascii = require("../config").ascii;
 
 client.chat.on("message", function(ev, msg) {
-	var match = msg.match(/\.a (.+)/);
+	var match = msg.match(/\.a(scii)? (.+)/);
 	var art;
-	if (match) art = ascii[match[1].toLowerCase()];
+	if (match) art = ascii[match[2].toLowerCase()];
 	if (match && art) {
 		client.replyMessage(ev, art);
-	} else if (msg == ".a") {
+	} else if (msg == ".a" || msg == ".ascii") {
 		client.replyMessage(ev, Object.keys(ascii).reduce(function(p, c) {
 			return p + ", " + c;
 		}));
