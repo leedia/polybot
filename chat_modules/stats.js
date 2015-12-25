0f6d1edf68138c0a547db2884b79da181d617887
Max Krieger <themaxaxis@gmail.com>
2015-12-24 20:45:06 -0500

    now segmentless

--- chat_modules/stats.js
+++ chat_modules/stats.js
@@ -4,14 +4,10 @@ client.chat.on("message", function(ev, msg){
 	if(msg == ".stats") {
 		var stats = client.getStats();
 		stats.created = new Date(stats.created);
-		var builder = new client.Client.MessageBuilder();
-		var segments = builder;
-		segments.bold("Statistics for Poly Bot since " + (parseInt(stats.created.getMonth()) + 1) + "/" + stats.created.getDate() + "/" + stats.created.getFullYear());
-		segments.linebreak();
-		segments.text("Messages: ").bold(stats.messages.toString());
-		segments.linebreak();
-		segments.text("Images: ").bold(stats.images.toString());
-		segments.toSegments();
-		client.replySegments(ev, segments);
+		var st = "";
+		st += "Statistics for Poly Bot since " + (parseInt(stats.created.getMonth()) + 1) + "/" + stats.created.getDate() + "/" + stats.created.getFullYear();
+		st += " Messages: " + stats.messages.toString();
+		st += " Images: " + stats.images.toString();
+		client.replyMessage(ev, st);
 	}
 });
